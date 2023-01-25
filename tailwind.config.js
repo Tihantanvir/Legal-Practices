14654b87445ee3a1202de0135be7fc3e591f03cf
Tihantanvir <tihantanvir@gamil.com>
2023-01-24 22:31:09 -0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    //...
+    plugins: [require("daisyui")],
+  }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
